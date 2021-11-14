513fe826b48ba1d86fd011f63821fecbab448137
Vukovic Emilie <emilievukovic79@gmail.com>
2021-11-14 12:30:37 +0100

    Add Error404 page

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Error404.css';
 
 const Error404 = () => {
-    return <div></div>;
+    return (
+        <main className="Main-Error404">
+            <div className="Content-404">
+                <span className="Number-404">404</span>
+                <p className="Text-404">
+                    Oups ! La page que vous demandez n'existe pas
+                </p>
+            </div>
+            <NavLink className="Back-Home" to="/">
+                Retourner sur la page d'accueil
+            </NavLink>
+        </main>
+    );
 };
 
 export default Error404;
