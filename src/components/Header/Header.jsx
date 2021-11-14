a29ba93a3ef5851096193c24317fe9030592bcc9
Vukovic Emilie <emilievukovic79@gmail.com>
2021-11-14 12:52:58 +0100

    Move the NavIcons component outside the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Header.css';
 import Navbar from '../Navbar/Navbar';
-import NavIcons from '../NavIcons/NavIcons';
+// import NavIcons from '../NavIcons/NavIcons';
 
 const Header = () => {
     return (
         <header>
             <Navbar />
-            <NavIcons />
+            {/* <NavIcons /> */}
         </header>
     );
 };
